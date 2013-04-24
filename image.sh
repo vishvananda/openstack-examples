c272eb53ba394c3ddfae1961d7112117b7c79d9c
Vishvananda Ishaya <vishvananda@gmail.com>
2013-04-24 08:07:48 -0700

    simplify image.sh

--- image.sh
+++ image.sh
@@ -4,7 +4,7 @@ OLD_ID=`glance details | grep "Name: test-image" -B3 | grep Id | cut -d' ' -f2`
 if [ "$OLD_ID" != "" ]; then
     glance image-delete $OLD_ID
 fi
-IMAGE="cirros-0.3.0-x86_64-disk.img"
-wget -nc http://launchpad.net/cirros/trunk/0.3.0/+download/$IMAGE
-glance image-create --name "test-image" --public --container-format bare --disk-format qcow2 < "$IMAGE"
+IMAGE="http://launchpad.net/cirros/trunk/0.3.0/+download/cirros-0.3.0-x86_64-disk.img"
+wget -nc $IMAGE
+glance image-create --name "test-image" --container-format bare --disk-format qcow2 < `basename $IMAGE`
 glance image-list
